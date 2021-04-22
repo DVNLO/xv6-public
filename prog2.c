f0712434f95c0b3c81aec4d94708320e1a9014f2
Dan Vyenielo <dvyenielo@protonmail.com>
2021-04-22 12:56:55 -0700

    add some print statements to the intial ticket allocation for each program.

--- prog2.c
+++ prog2.c
@@ -5,13 +5,14 @@
 int main(int argc, char *argv[])
 {
   int const tickets = set_tickets(20);
+  printf(1, "prog2 : tickets == %d\n", tickets);
   int i,k;
   const int loop=43000;
   for(i=0;i<loop;i++) {
-    asm(nop);
+    asm("nop");
     //in order to prevent the compiler from optimizing the for loop
     for(k=0;k<loop;k++) {
-      asm(nop);
+      asm("nop");
     }
   }
   exit();
