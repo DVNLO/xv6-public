0b57cd7484b24787be400c7328b0f5f880ac25a2
Dan Vyenielo <dvyenielo@protonmail.com>
2021-05-25 12:30:10 -0700

    update

--- lock.h
+++ lock.h
@@ -8,8 +8,27 @@ typedef struct
     uint is_locked;
 } lock_t;
 
-void lock_init(lock_t * const lk);
-void lock_acquire(lock_t * const lk);
-void lock_release(lock_t * const lk);
+void
+lock_init(lock_t * const lk)
+{
+    lk->is_locked = 0U;
+}
+
+void
+lock_acquire(lock_t * const lk)
+{
+    while(xchg(&lk->is_locked, 1) != 0)
+    {
+        continue;
+    }
+    printf(1, "acquired\n");
+}
+
+void
+lock_release(lock_t * const lk)
+{
+    xchg(&lk->is_locked, 0);
+    printf(1, "released\n");
+}
 
 #endif
