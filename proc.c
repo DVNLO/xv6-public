b081c5a6b8b3b51b594825853ff0c94c4b80ff2a
Dan Vyenielo <dvyenielo@protonmail.com>
2021-05-25 10:00:46 -0700

    update

--- proc.c
+++ proc.c
@@ -559,7 +559,7 @@ clone(void * stack, int size)
   cprintf("clone : ustack[1] = %p\n", ustack[1]);
   cprintf("clone : ustack[2] = %p\n", ustack[2]);
   new_proc->tf->ebp = (uint)(stack) + (uint)(size);
-  new_proc->tf->esp = (uint)(&ustack[1]);
+  new_proc->tf->esp = (uint)(&ustack[0]);
   new_proc->tf->eip = ustack[2];
   uint i;
   for(i = 0; i < NOFILE; i++)
